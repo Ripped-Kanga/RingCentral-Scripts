16580cafe2080d8483316d6a49b690c67e3f126a
Alan Saunders <ripped_kanga@icould.com>
2025-07-08 19:40:21 +0930

    Initial Commit UserAudit

---
+++ Audit-Scripts/UserAudit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+"""
+Author:   Alan Saunders
+Purpose:  Uses the RingCentral API to collect information on the RingCentral instance, useful for conducting audits and health checks on RingCentral instances.
+Version:  0.4
+Github:   https://github.com/Ripped-Kanga/RingCentral-Scripts
+"""
+# Import libraries
