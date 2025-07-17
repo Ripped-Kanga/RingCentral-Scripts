76af477f94abee138791e0dcd5ba1581745ae1e6
Alan Saunders <ripped_kanga@icould.com>
2025-07-17 22:55:46 +0930

    increment version

--- Audit-Scripts/RingCentralMain.py
+++ Audit-Scripts/RingCentralMain.py
@@ -3,7 +3,7 @@
 """
 Author:   Alan Saunders
 Purpose:  Uses the RingCentral API to collect information on the RingCentral instance, useful for conducting audits and health checks on RingCentral instances.
-Version:  0.4
+Version:  0.5
 Github:   https://github.com/Ripped-Kanga/RingCentral-Scripts
 """
 # Import libraries
