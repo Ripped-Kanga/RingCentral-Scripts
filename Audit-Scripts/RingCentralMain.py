df93062f12f90705c5a8a1a86e6d4d5e969f7ff9
Alan Saunders <ripped_kanga@icould.com>
2025-07-25 23:03:34 +0930

    Fixed filter params on audit_checker().

--- Audit-Scripts/RingCentralMain.py
+++ Audit-Scripts/RingCentralMain.py
@@ -101,13 +101,13 @@ def audit_checker (audit_url):
 
         built_url = str(f'/restapi/v1.0/account/~/extension?perPage={totalElements}&Type=User&{query_option}')
         filter_user_count = connectRequest(built_url).json().paging.totalElements
-        filter_user_built_url = str(f'/restapi/v1.0/account/~/extension?perPage={filter_user_count}&Type=User&{query_option}')
+        filter_user_built_url = str(f'/restapi/v1.0/account/~/extension?perPage={filter_user_count}&type=User&{query_option}')
         print (filter_user_count)
         return (filter_user_count, totalElements, filter_user_built_url)
 
       elif ask_audit.lower() == "n":
         print("No customisation will apply to the user audit, proceeding.")
-        built_url = str(f'/restapi/v1.0/account/~/extension?perPage={totalElements}')
+        built_url = str(f'/restapi/v1.0/account/~/extension?perPage={totalElements}&type=User')
         filter_user_count = False
         return (filter_user_count, totalElements, built_url)
 
