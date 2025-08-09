54f4121b8dcccea3c381800453502c9a3b0c7d4b
Alan Saunders <ripped_kanga@icould.com>
2025-08-09 14:29:21 +0930

    added housekeeping() call.

--- Audit-Scripts/CallQueueAudit.py
+++ Audit-Scripts/CallQueueAudit.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python
-"""
-Author:   Alan Saunders
-Purpose:  Uses the RingCentral API to collect information on the RingCentral Call Queues.
-Version:  0.5
-Github:   https://github.com/Ripped-Kanga/RingCentral-Scripts
-"""
+
 # Import libraries
 import os
 import sys
@@ -12,7 +7,7 @@ import json
 import time
 import datetime
 import csv
-from RingCentralMain import connection_test, connectRequest, audit_checker
+from RingCentralMain import housekeeping, connection_test, connectRequest, audit_checker
 import pprint
 
 # Global Variables
@@ -22,6 +17,7 @@ start_time = datetime.datetime.now()
 
 # Start main thread
 def main_callqueue():
+	housekeeping()
 	print (f'Script Start Time: {start_time}')
 	connection_attempt = connection_test()
 	if connection_attempt:
