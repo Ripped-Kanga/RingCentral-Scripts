062bbbcddefc8186ddfff604dc27be43829d3bfe
Alan Saunders <ripped_kanga@icould.com>
2025-08-12 19:09:12 +0930

    Added business hours rule termination destination (voicemail, int or ext forward)

--- Audit-Scripts/UserAudit.py
+++ Audit-Scripts/UserAudit.py
@@ -48,9 +48,11 @@ def get_ringcentral_users(filter_user_count, user_count, built_url):
 		device_resp = connectRequest(f'/restapi/v1.0/account/~/extension/{record.id}/device')
 		user_role_resp = connectRequest(f'/restapi/v1.0/account/~/extension/{record.id}/assigned-role')
 		user_presence_resp = connectRequest(f'/restapi/v1.0/account/~/extension/{record.id}/presence')
+		user_forwarding_resp = connectRequest(f'/restapi/v1.0/account/~/extension/{record.id}/answering-rule/business-hours-rule')
 		user_data = json.loads(user_resp.text())
 		device_data = json.loads(device_resp.text())
 		user_presence_data = json.loads(user_presence_resp.text())
+		user_forwarding_data = json.loads(user_forwarding_resp.text())
 		device_records = device_data['records']
 		user_roles_list = user_role_resp.json().records
 		if user_roles_list:
@@ -58,7 +60,8 @@ def get_ringcentral_users(filter_user_count, user_count, built_url):
 				ext_assigned_role = roles.displayName
 		else:
 			ext_assigned_role = ""
-
+		
+		# Set variables for dict build.
 		ext_id = record.id
 		ext_name = record.name
 		ext_number = record.extensionNumber
@@ -72,48 +75,72 @@ def get_ringcentral_users(filter_user_count, user_count, built_url):
 		ext_setup_wizard = user_data.get('setupWizardState')
 		ext_dnd_status = user_presence_data.get('dndStatus')
 
+		# Check if extension has business hours rule
+		bhr_missed_call_forward = user_forwarding_data.get('missedCall')
+		bhr_voicemail = user_forwarding_data.get('voicemail', {}).get('enabled')
+
+		if bhr_voicemail == True:
+			ext_bhr_fw_dest = 'User Voicemail'
+
+		elif bhr_missed_call_forward:
+			# check if it is an internal or external forward
+			dest_type = user_forwarding_data.get('missedCall', {}).get('actionType')
+			if dest_type == 'ConnectToExtension':
+				ext_bhr_int_fw_id = user_forwarding_data.get('missedCall', {}).get('extension', {}).get('id')
+				check_fw_destination_int_name = connectRequest(f'/restapi/v1.0/account/~/extension/{ext_bhr_int_fw_id}')
+				fw_destination_int_name_data = json.loads(check_fw_destination_int_name.text())
+				ext_bhr_fw_dest = fw_destination_int_name_data.get('name')
+
+			elif dest_type == 'ConnectToExternalNumber':
+				ext_bhr_fw_dest = user_forwarding_data.get('missedCall', {}).get('externalNumber', {}).get('phoneNumber')
+
+		else:
+			ext_bhr_fw_dest = ""
+
 		# Store user and device audit data, if the user has a device, store the device values, otherwise store blank device values.
 		if device_records:
 			for device in device_records:
 				row = {
-					"User ID":							ext_id,
-					"Extension Name":				ext_name,
-					"Extension Number":			ext_number,
-					"Extension Status":			ext_status,
-					"Site":									ext_site,
-					"Company":							ext_company,
-					"Department":						ext_department,
-					"Job Title":						ext_job_title,
-					"Email":								ext_email,
-					"DND Status":						ext_dnd_status,
-					"User Assigned Role":		ext_assigned_role,
-					"is Administrator?":		ext_is_admin,
-					"Setup Wizard State":		ext_setup_wizard,
-					"Device Name":					device.get('name'),
-					"Device Model":					device.get('model', {}).get('name'),
-					"Device Serial":				device.get('serial'),
-					"Device Status":				device.get('status')
+					"User ID":														ext_id,
+					"Extension Name":											ext_name,
+					"Extension Number":										ext_number,
+					"Extension Status":										ext_status,
+					"Site":																ext_site,
+					"Company":														ext_company,
+					"Department":													ext_department,
+					"Job Title":													ext_job_title,
+					"Email":															ext_email,
+					"DND Status":													ext_dnd_status,
+					"Business Hours Forward Destination":	ext_bhr_fw_dest,
+					"User Assigned Role":									ext_assigned_role,
+					"is Administrator?":									ext_is_admin,
+					"Setup Wizard State":									ext_setup_wizard,
+					"Device Name":												device.get('name'),
+					"Device Model":												device.get('model', {}).get('name'),
+					"Device Serial":											device.get('serial'),
+					"Device Status":											device.get('status')
 				}
 				datalist.append(row)
 		else:
 			row = {
-			"User ID":							ext_id,
-			"Extension Name":				ext_name,
-			"Extension Number":			ext_number,
-			"Extension Status":			ext_status,
-			"Site":									ext_site,
-			"Company":							ext_company,
-			"Department":						ext_department,
-			"Job Title":						ext_job_title,
-			"Email":								ext_email,
-			"DND Status":						ext_dnd_status,
-			"User Assigned Role":		ext_assigned_role,
-			"is Administrator?":		ext_is_admin,
-			"Setup Wizard State":		ext_setup_wizard,
-			"Device Name":					"",
-			"Device Model":					"",
-			"Device Serial":				"",
-			"Device Status":				""
+			"User ID":														ext_id,
+			"Extension Name":											ext_name,
+			"Extension Number":										ext_number,
+			"Extension Status":										ext_status,
+			"Site":																ext_site,
+			"Company":														ext_company,
+			"Department":													ext_department,
+			"Job Title":													ext_job_title,
+			"Email":															ext_email,
+			"DND Status":													ext_dnd_status,
+			"Business Hours Forward Destination":	ext_bhr_fw_destination_int_name,
+			"User Assigned Role":									ext_assigned_role,
+			"is Administrator?":									ext_is_admin,
+			"Setup Wizard State":									ext_setup_wizard,
+			"Device Name":												"",
+			"Device Model":												"",
+			"Device Serial":											"",
+			"Device Status":											""
 		}
 			datalist.append(row)
 		
@@ -129,8 +156,8 @@ def get_ringcentral_users(filter_user_count, user_count, built_url):
 			print (f'### {ext_name} ###')
 			pprint.pprint(row, indent=4, sort_dicts=False)
 
-	#Parse the datalist dictionary to be written to csv file
-	build_user_csv(datalist)
+		#Parse the datalist dictionary to be written to csv file
+		build_user_csv(datalist)
 
 #Builds the csv file, sets headers.
 def build_user_csv(datalist):
