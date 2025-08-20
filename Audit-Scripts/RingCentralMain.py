6002dfdb02315e3b0e01500d643bf833cfa7ec53
Alan Saunders <ripped_kanga@icould.com>
2025-08-21 00:10:12 +0930

    Implemented API filtering control for user. Can now call all filter fields, remaining parameters to be added in next commit. Added logging.

--- Audit-Scripts/RingCentralMain.py
+++ Audit-Scripts/RingCentralMain.py
@@ -3,6 +3,7 @@
 __author__ = "Alan Saunders"
 __purpose__ = "Uses the RingCentral API to collect information on the RingCentral instance, useful for conducting audits and health checks on RingCentral instances."
 __core_version__ = "0.95 - alpha 2"
+__version__ = "0.7"
 __github__ = "https://github.com/Ripped-Kanga/RingCentral-Scripts\n"
 __disclaimer__ = "The purpose of this project is to provide easy auditability to the RingCentral platform. All the API calls made in this project are GET requests and represent no danger to the RingCentral data. To exit the script at any time, use CTRL + C. All data collected by this tool is writen to CSV file, the file is stored in the /AuditResults folder."
 
@@ -16,13 +17,25 @@ import csv
 import textwrap
 from dotenv import load_dotenv
 from ringcentral import SDK
+from ringcentral.http.api_exception import ApiException
+from urllib.parse import urlencode
 import inspect #Used for tracing stack
 from pick import pick #Pick Menu
+import logging
 load_dotenv()
 
 # Global Variables
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', '0').lower() in ('1', 'true', 'yes')
 
+# Enable Logging
+logging.basicConfig(
+	level=logging.DEBUG if DEBUG else logging.INFO,
+	format='%(asctime)s [%(levelname)s] %(message)s',
+	handlers=[
+		logging.StreamHandler(),
+		logging.FileHandler("RingCentral_Auditor.log", mode='a')
+		]
+	)
 
 # RingCentral SDK
 rcsdk = SDK( os.environ.get('RC_APP_CLIENT_ID'),
@@ -54,32 +67,53 @@ def connectRequest(url):
 			# Set Header variables
 			http_status = resp.response().status_code
 			headers = resp.response().headers
-			api_limit = int(headers["X-Rate-Limit-Limit"])
-			api_limit_remaining = int(headers["X-Rate-Limit-Remaining"])
-			api_limit_window = int(headers["X-Rate-Limit-Window"])
+			api_limit = int(headers.get("X-Rate-Limit-Limit", 1))
+			api_limit_remaining = int(headers.get("X-Rate-Limit-Remaining", 1))
+			api_limit_window = int(headers.get("X-Rate-Limit-Window", 1))
 			# Check API limit on each request, if API limit hits 0, sleep the script for the time reported by API Limit Window.
 			if api_limit_remaining == 0:
-				retry_after = api_limit_window
-				print(f'Rate limit has been hit, waiting for {retry_after} seconds, the script will automatically resume.')
-				time.sleep(retry_after)
+				logging.warning(f'Rate limit has been hit, waiting for {api_limit_window} seconds, the script will automatically resume.')
+				time.sleep(api_limit_window)
 				continue
 			else:
 				return resp
 
-		except Exception as e:
-			error_message = str(e)
+		except ApiException as e:
+			api_response = e.api_response()
+			status_code = 0
+			error_text = str(e)
+			error_body = error_text
 
-			if "HTTP 404" in error_message:
-				if DEBUG == True:
-					print(f'404 Not Found for URL: {url}')
-					return None
-				else:
-					return None
-					
+			if api_response:
+				try:
+					response = api_response.response()
+					if response is not None:
+						status_code = response.status_code
+						error_body = response.text
+				except Exception as parse_error:
+					logging.debug(f'Failed to parse API response: {parse_error}')
+
+			# Catch 404 errors returned due to API returned data None
+			if status_code == 404:
+				logging.info(f'404 Not Found for URL {url}')
+				return None
+			# Catch Answering Rule errors for elements that don't support the feature. 
+			elif "AWR-193" in error_body:
+				logging.info(f'HTTP Error 400, AWR-193 Answering Rule not supported')
+				return None
+			# Catch server 500 errors and sleep for 5 seconds to.
+			elif status_code >= 500:
+				logging.error(f'Server error {status_code}, retrying after short delay...')
+				time.sleep(5)
+				continue
+			# Catch any unhandled API error.
 			else:
-				print(f'Unexpected API error for URL {url}:\n{error_message}')
+				logging.error(f'Unhandled API error ({status_code}) at {url}:{error_body}')
 				return None
 
+		except Exception as e:
+			logging.critical(f'Non-API Error Occurred {str(e)}')
+			sys.exit()
 
 # Performs a connection test to the RingCentral API and returns True if the response is HTTP 200, also retrieves and prints the company info to console.
 def connection_test():
@@ -89,7 +123,7 @@ def connection_test():
 
 		if connect_test_url.response().status_code == 200:
 			print(f'Connection returned 200 OK, displaying company information.\n')
-			print (f'#'*27, '\n### Company Information ###\n','#'*27, sep="")
+			print (f'#'*80, '\n\u25BA\u25BA\u25BA Company Information\n','#'*80, sep="")
 			
 			# Retrieve company info, service plan, billing
 			company_info = json.loads(connect_test_url.text())
@@ -115,7 +149,7 @@ def connection_test():
 				f'Company Address - {company_address_street}, {company_address_city}, {company_address_state}, {company_address_zip}, {company_address_country}\n',
 				sep='\n'
 				)
-			print (f'#'*29, '\n### Plan and Billing Info ###\n','#'*29, sep="")
+			print (f'#'*80, '\n\u25BA\u25BA\u25BA Plan and Billing Info\n','#'*80, sep="")
 			print (
 				f'RingCentral Plan: {rc_plan_type}',
 				f'Billing Schedule: {rc_billing_schedule}\n',
@@ -144,56 +178,113 @@ def audit_checker (audit_url):
 
 		#Invoked if the calling script is UserAudit.py
 		if calling_function == "main_user":
-			print (f'Found {totalElements} Users:\n')
-			
+			print ('#'*80, f'\n\u25BA\u25BA\u25BA Found {totalElements} Users\n', '#'*80, sep="")
+			print ()
 			while True:
-				ask_audit = str(input("Do you want to customise the API Filter Parameters?(y/n: "))
+				api_filter_info = "You can customise the filter parameters to modify the returned users from RingCentral. Note that most elements in RingCentral count as users (Users, Message-Only Extensions, Call Queues, IVR Menus, ect). If you select (n) no filter parameter will be applied, returning all users in the RingCentral system including the afore mentioned elements."
+				wrapped_api_filter_info = textwrap.fill(api_filter_info, width=80)
+				print (wrapped_api_filter_info)
+				print ()
+				ask_audit = str(input("Do you want to customise the API Filter Parameters?(y/n): "))
 				if ask_audit in ['y', 'n']:
 					break
-				print("Invalid input. Please enter 'y' or 'n'.")
+				print ("Invalid input. Please enter 'y' or 'n'.")
 
 			if ask_audit == "y":
 				print("API Filter Parameter custimisation selected...\n")
-				time.sleep(3)
+				time.sleep(1)
 				# Load pick menu
 				title = 'Select a query option below: (You can only choose one!)'
-				query_options = ['extensionNumber', 'email', 'status']
-				option, index = pick(query_options, title, indicator='\u25BA\u25BA')
+				query_options = ['Type', 'Extension Number', 'Email', 'Status']
+				list_option, index = pick(query_options, title, indicator='\u25BA\u25BA')
 				
 				while True:
-					match option:
-						case 'extensionNumber':
-							while True:
-								query_extension = input("Enter the Extension Number: ")
-								if query_extension.isdigit():
-									break
-								print ("Please only enter a numeric extension number!")
-							query_option = str(f"{option}="+str(query_extension))
-						case 'email':
-							query_email = input("Enter the Email Address: ")
-							query_option = (f'{option}='+query_email)
-						case 'status':
-							while True:
-								query_status = str(input("Enter the User Status (Enabled, Disabled, NotActivated, Unassigned: "))
-								if query_status in ['Enabled', 'Disabled', 'NotActivated', 'Unassigned']:
-									break
-								print ("Invalid input, please review the options and try again.\n")
-							query_option = str(f'{option}='+query_status)
-						case _:
-							sys.exit("An error occured in the pick list.")
-
-					built_url = str(f'/restapi/v1.0/account/~/extension?perPage={totalElements}&type=User&{query_option}')
-					filter_user_count = connectRequest(built_url).json().paging.totalElements
-					filter_user_built_url = str(f'/restapi/v1.0/account/~/extension?perPage={filter_user_count}&type=User&{query_option}')
+					if list_option == 'Type':
+						# Load pick menu, multi-select returns Tuple
+						title = 'Select (SPACE) what extension type you want to filter for. Press ENTER to continue:'
+						extension_types_list = [
+						'User',
+						'Call Queue',
+						'Announcement',
+						'Voicemail'
+						]
+						extension_types= []
+						ext_options = pick(extension_types_list, title, multiselect=True, min_selection_count=1, indicator='\u25BA\u25BA')
+						for option, _ in ext_options:
+							o = option.strip()
+							print(f"Checking option: '{o}'")
+							if o == 'User':
+								extension_types.append("User")
+							elif o == 'Call Queue':
+								extension_types.append("Department")
+							elif o == 'Announcement':
+								extension_types.append("Announcement")
+							elif o == 'Voicemail':
+								extension_types.append("Voicemail")
+							else:
+								sys.exit(f"Unrecognized option: '{o}'")
+						queryParams = urlencode({'type': extension_types}, doseq=True)
 					
+					elif list_option == 'Extension Number':
+						while True:
+							query_extension = input("Enter the Extension Number: ")
+							if query_extension.isdigit():
+								break
+							print ("Please only enter a numeric extension number!")
+						queryParams = urlencode({'extensionNumber': query_extension}, doseq=True)
+					
+					elif list_option == 'Email': # need to add seperation via comma, multiple values.
+						query_email = input("Enter the Email Address: ")
+						queryParams = urlencode({'email': query_email}, doseq=True)
+					
+					elif list_option == 'Status':
+						# Load pick menu, multi-select returns Tuple
+						title = 'Select (SPACE) what extension type you want to filter for. Press ENTER to continue:'
+						extension_status = [
+						'Enabled',
+						'Disabled',
+						'Frozen',
+						'Not Activated',
+						'Unassigned'
+						]
+						status_types= []
+						status_options = pick(extension_status, title, multiselect=True, min_selection_count=1, indicator='\u25BA\u25BA')
+						for option, _ in status_options:
+							o = option.strip()
+							print(f"Checking option: '{o}'")
+							if o == 'Enabled':
+								status_types.append("Enabled")
+							elif o == 'Disabled':
+								status_types.append("Disabled")
+							elif o == 'Frozen':
+								status_types.append("Frozen")
+							elif o == 'Not Activated':
+								status_types.append("NotActivated")
+							elif o == 'Unassigned':
+								status_types.append("Unassigned")
+							else:
+								sys.exit(f"Unrecognized option: '{o}'")
+						queryParams = urlencode({'status': status_types}, doseq=True)
+					
+					else:
+						sys.exit('An error occured while picking filters')
+
+					built_url = f'/restapi/v1.0/account/~/extension?{queryParams}'
+					filter_user_count = connectRequest(built_url).json().paging.totalElements
+					print (filter_user_count)
+					filter_user_built_url = str(f'/restapi/v1.0/account/~/extension?perPage={filter_user_count}&{queryParams}')
+
 					if filter_user_count:
 						print (f'Found {filter_user_count} users with current filter parameters, proceeding to field customisation.\n')
 						return (filter_user_count, totalElements, filter_user_built_url) 
 					print ("No results returned from filter paramaters. Please review your filter paramaters and try again.\n")
+					time.sleep(2)
 
 			elif ask_audit == "n":
 				print("No customisation to the API Filter Parameters selected, proceeding...\n")
-				built_url = str(f'/restapi/v1.0/account/~/extension?perPage={totalElements}&type=User')
+				built_url = str(f'/restapi/v1.0/account/~/extension?perPage={totalElements}')
+				if DEBUG == True:
+					print (f'DEBUG - {built_url}')
 				filter_user_count = False
 				return (filter_user_count, totalElements, built_url)
 
@@ -247,6 +338,9 @@ def audit_checker (audit_url):
 			elif ask_audit == 'n':
 				print ("No customisation to the API Filter Parameters selected, proceeding...")
 				built_url = f'/restapi/v2/accounts/~/phone-numbers?perPage={totalElements}'
+				if DEBUG == True:
+					print(built_url)
+
 				filtered_phone_number_count = False
 				return (filtered_phone_number_count, totalElements, built_url)
 	
